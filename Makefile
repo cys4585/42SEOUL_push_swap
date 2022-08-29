56e673ac13853ec1d4645f62d2c5a3549bc643e2
youngcho <soc4585@naver.com>
2022-08-29 16:03:01 +0900

    Makefile init

--- Makefile
+++ Makefile
@@ -6,9 +6,39 @@
 #    By: youngcho <youngcho@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/08/29 15:45:33 by youngcho          #+#    #+#              #
-#    Updated: 2022/08/29 15:45:50 by youngcho         ###   ########.fr        #
+#    Updated: 2022/08/29 16:02:02 by youngcho         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME = push_swap
 
+CC = cc
+CFLAGS = -Wall -Wextra -Werror
+
+SRCS_DIR = srcs/
+OBJS_DIR = objs/
+INCS_DIR = incs/
+
+SRCS_NAME = push_swap.c
+OBJS_NAME = $(SRCS_NAME:.c=.o)
+
+SRCS = $(addprefix $(SRCS_DIR), $(SRCS_NAME))
+OBJS = $(addprefix $(OBJS_DIR), $(OBJS_NAME))
+
+.PHONY: all clean fclean re
+all : $(NAME)
+clean :
+	rm -rf $(OBJS_DIR)
+fclean : clean
+	rm -f $(NAME)
+re : fclean
+	make all
+
+$(NAME) : $(OBJS)
+	$(CC) $(CFLAGS) $(OBJS) -o $@
+
+$(OBJS_DIR)%.o : $(SRCS_DIR)%.c | $(OBJS_DIR)
+	$(CC) $(CFLAGS) -c  $< -o $@
+
+$(OBJS_DIR) :
+	mkdir $(OBJS_DIR)
--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -1,19 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: youngcho <youngcho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 15:46:46 by youngcho          #+#    #+#             */
-/*   Updated: 2022/08/29 15:51:09 by youngcho         ###   ########.fr       */
+/*   Updated: 2022/08/29 16:01:02 by youngcho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 int	main(int argc, char *argv[])
 {
+	printf("%d %p\n", argc, argv);
 	// valiation argument
 	// make stack
 	// merge sort
-	returrn (0);
+	return (0);
 }
