64dc488a2783d219c0a0897a888909f74dfdcd23
youngcho <soc4585@naver.com>
2022-08-29 15:55:38 +0900

    style: 메인소스파일 이름 수정

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,11 +6,14 @@
 /*   By: youngcho <youngcho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 15:46:46 by youngcho          #+#    #+#             */
-/*   Updated: 2022/08/29 15:47:07 by youngcho         ###   ########.fr       */
+/*   Updated: 2022/08/29 15:51:09 by youngcho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	main(int argc, char *argv[])
 {
+	// valiation argument
+	// make stack
+	// merge sort
 	returrn (0);
 }
