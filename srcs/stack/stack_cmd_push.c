6083089cc9f807268485c22a5ce70d493b305b52
youngcho <soc4585@naver.com>
2022-08-31 12:02:00 +0900

    pop 예외처리 (요소 없을 때 pop 하면 segmentation fault)

--- srcs/stack/stack_cmd_push.c
+++ srcs/stack/stack_cmd_push.c
@@ -6,18 +6,23 @@
 /*   By: youngcho <youngcho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/30 18:47:27 by youngcho          #+#    #+#             */
-/*   Updated: 2022/08/30 18:56:05 by youngcho         ###   ########.fr       */
+/*   Updated: 2022/08/31 12:01:05 by youngcho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "stack_utils.h"
+#include <stddef.h>
 
 void	pa(t_stack *a)
 {
+	if (a->top == NULL)
+		return ;
 	push(a, pop(a));
 }
 
 void	pb(t_stack *b)
 {
+	if (a->top == NULL)
+		return ;
 	push(b, pop(b));
 }
